45106b17baae96ffba784943749def572c98304e
Jorge Corea <jcorear94@outlook.com>
2019-09-27 12:56:01 -0600

    stuff

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,10 @@
-$(document).on('click','#menu-toggle', function() { 
-    $('body').toggleClass('nav-opened');
+jQuery(document).on('click','#menu-toggle', function() { 
+    jQuery('body').toggleClass('nav-opened');
 });
 
-$(document).on('click', ".menu-item-has-children > a", t => {
-    $parent = $(t.target).parent();
-    $(t.target).hasClass('expand-toggle') && (t.preventDefault(), $parent.hasClass("opened") ? $parent.removeClass("opened") : ($(".menu-item-has-children > a").removeClass("opened"), $parent.addClass("opened")));
+jQuery(document).on('click', ".menu-item-has-children > a", t => {
+    $parent = jQuery(t.target).parent();
+    jQuery(t.target).hasClass('expand-toggle') && (t.preventDefault(), $parent.hasClass("opened") ? $parent.removeClass("opened") : (jQuery(".menu-item-has-children > a").removeClass("opened"), $parent.addClass("opened")));
 });
+
 
